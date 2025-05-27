63ce84a19c65b0efde45f01300ccdeb33031451c
VimalanKM <VimalanM@ami.com>
2025-05-27 15:19:44 -0400

    changed red color scheme

--- vimSearch.py
+++ vimSearch.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -10,6 +10,10 @@ def index():
 def search():
     query = request.form.get('query')
     return render_template('results.html', query=query)
+
+@app.route('/authorization-code/callback')
+def callback():
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
